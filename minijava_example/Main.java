aee0c6fcfb1f924fbfe7e26df6ec608ec1ac4a1d
panosdip <panospanagiotis2004@gmail.com>
2026-05-17 11:25:57 +0300

    Main multiple files

--- minijava_example/Main.java
+++ minijava_example/Main.java
@@ -5,49 +5,55 @@ import syntaxtree.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        if(args.length != 1){
+        if(args.length < 1){
             System.err.println("Usage: java Main <inputFile>");
             System.exit(1);
         }
 
 
 
-        FileInputStream fis = null;
-        try{
-            fis = new FileInputStream(args[0]);
-            MiniJavaParser parser = new MiniJavaParser(fis);
 
-            Goal root = parser.Goal();
+        for(String filename : args){
+            System.out.println("Processing file: " + filename);
 
-            System.err.println("Program parsed successfully.");
+            FileInputStream fis = null;
 
-            System.err.println("------Visitor 1-----");
-
-            MyVisitor visitor1 = new MyVisitor();
-            root.accept(visitor1, null);
-
-            System.err.println("------Visitor 2-----");
-
-            Visitor2 visitor2 = new Visitor2(visitor1.symbolTable);
-            root.accept(visitor2, null);
-
-            System.err.println("------Visitor 3-----");
-            Visitor3 visitor3 = new Visitor3(visitor2.symbolTable);
-            root.accept(visitor3, null);
-
-        }
-        catch(ParseException ex){
-            System.out.println(ex.getMessage());
-        }
-        catch(FileNotFoundException ex){
-            System.err.println(ex.getMessage());
-        }
-        finally{
             try{
-                if(fis != null) fis.close();
+                fis = new FileInputStream(filename);
+                MiniJavaParser parser = new MiniJavaParser(fis);
+
+                Goal root = parser.Goal();
+
+                System.err.println("Program parsed successfully.");
+
+                System.err.println("------Visitor 1-----");
+
+                MyVisitor visitor1 = new MyVisitor();
+                root.accept(visitor1, null);
+
+                System.err.println("------Visitor 2-----");
+
+                Visitor2 visitor2 = new Visitor2(visitor1.symbolTable);
+                root.accept(visitor2, null);
+
+                System.err.println("------Visitor 3-----");
+                Visitor3 visitor3 = new Visitor3(visitor2.symbolTable);
+                root.accept(visitor3, null);
+
             }
-            catch(IOException ex){
+            catch(ParseException ex){
+                System.out.println(ex.getMessage());
+            }
+            catch(FileNotFoundException ex){
                 System.err.println(ex.getMessage());
+            }
+            finally{
+                try{
+                    if(fis != null) fis.close();
+                }
+                catch(IOException ex){
+                    System.err.println(ex.getMessage());
+                }
             }
         }
     }
